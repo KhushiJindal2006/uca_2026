491c24e70ae4de881b963edca301284404e1dfc3
Khushi Jindal <khushi1198.becse24@chitkara.edu.in>
2026-07-15 11:59:01 +0000

    Added awk practice tasks

---
+++ awk_tasks.sh
@@ -0,0 +1,17 @@
+awk -F',' '{print $1,$2}' employees.txt
+
+echo
+
+awk -F',' '$2=="Engineering" {print}' employees.txt
+
+echo
+
+awk -F',' '{print $1,"gross pay: $" $3*$4}' employees.txt
+
+echo
+
+awk -F',' '{print NR,"("NF" fields):",$0}' employees.txt
+
+echo
+
+awk -F',' 'BEGIN{total=0}$2=="Engineering"{total += $3*$4} END {print "Total Engineering Payroll: $" total}' employees.txt

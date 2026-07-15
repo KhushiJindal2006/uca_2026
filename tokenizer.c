d359d56f826efdb245f9bd912228484b9fc17bd5
Khushi Jindal <khushi1198.becse24@chitkara.edu.in>
2026-07-15 11:14:36 +0000

    Added strtok() example

---
+++ tokenizer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char string[]="C,C++,Java,Python,Rust";
+	char *token=strtok(string,",");
+
+	while(token!=NULL){
+		printf("%s\n",token);
+		token=strtok(NULL,",");
+	}
+	return 0;
+}
